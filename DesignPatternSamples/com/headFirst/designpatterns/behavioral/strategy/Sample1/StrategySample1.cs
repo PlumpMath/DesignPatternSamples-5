2242a234c61714da79cf919c6953a43880724842
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-05-23 17:38:21 +0530

    Commiting StrategySample1

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/strategy/Sample1/StrategySample1.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.Strategy.Sample1
+{
+    class StrategySample1
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
