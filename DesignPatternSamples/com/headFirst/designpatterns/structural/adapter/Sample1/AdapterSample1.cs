570ed5f7016a087742ad0145f7bcd96b332fe51f
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-05-31 16:40:34 +0530

    Added Empty Project for Adapter Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/structural/adapter/Sample1/AdapterSample1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.HeadFirst.DesignPatterns.Structural.Adapter.Sample1
+{
+    class AdapterSample1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
