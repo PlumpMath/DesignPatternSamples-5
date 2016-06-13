3ae03834e28e0f89e0b561fef38a04e5cc36430a
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 16:47:11 +0530

    Added Coffee class to Project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    class Coffee : CoffeeBeverage
+    {
+        protected override void addCondiments()
+        {
+            Console.WriteLine("Adding sugar and milk");
+        }
+
+        protected override void brew()
+        {
+            Console.WriteLine("Dripping coffee through filter");
+        }
+    }
+}
