7fd3b485bf1e61a0143a8e6620586ca147be121b
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 19:39:31 +0530

    Added TeaWithHook class in the Project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/TeaWithHook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    class TeaWithHook : CoffeeBeverageWithHook
+    {
+        protected override void addCondiments()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+
+        protected override void brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        protected override bool customerWantsCondiments()
+        {
+            var ans = getUserInput();
+
+            if (ans.ToLower().StartsWith("y"))
+                return true;
+            else
+                return false;
+        }
+
+        private string getUserInput()
+        {
+            var answer = string.Empty;
+
+            Console.Write("Would you like lemon with your tea (y/n)? ");
+
+            try
+            {
+                answer = Console.ReadLine();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("IO error trying to read your answer");
+            }
+
+            if (string.IsNullOrWhiteSpace(answer))
+                return "no";
+
+            return answer;
+        }
+
+
+    }
+}
