a6827231058b152601522ab1f6b434f1ffc61cf6
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 19:17:54 +0530

    Added Abstract class CoffeeBeverageWithHook class to demonstrate hook method concept in Template Method Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/CoffeeBeverageWithHook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    abstract class CoffeeBeverageWithHook : ICoffeeBeverage
+    {
+        public void AddCondiments()
+        {
+            addCondiments();
+        }
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        public void Brew()
+        {
+            brew();
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+
+            if (customerWantsCondiments())
+                AddCondiments();
+        }
+
+        protected abstract void brew();
+
+        protected abstract void addCondiments();
+
+        //Hook method
+        protected virtual bool customerWantsCondiments()
+        {
+            return true;
+        }
+    }
+}
