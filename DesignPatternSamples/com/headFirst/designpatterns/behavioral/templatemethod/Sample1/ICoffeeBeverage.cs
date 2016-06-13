7c06188b460581345fe1c849bb468a909d3d4675
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 16:32:48 +0530

    Added Interface ICoffeeBeverage in Sample Project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/ICoffeeBeverage.cs
@@ -0,0 +1,15 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    interface ICoffeeBeverage
+    {
+        void PrepareRecipe();
+
+        void BoilWater();
+
+        void Brew();
+
+        void PourInCup();
+
+        void AddCondiments();
+    }
+}
