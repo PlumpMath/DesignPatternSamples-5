33db8348126dd5feba742182075ae00438b8546f
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 16:41:43 +0530

    Added abstract class CoffeeBeverage to project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/CoffeeBeverage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    abstract class CoffeeBeverage : ICoffeeBeverage
+    {
+        public void AddCondiments()
+        {
+            addCondiments();
+        }
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        public void Brew()
+        {
+            brew();
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+
+        protected abstract void brew();
+
+        protected abstract void addCondiments();
+    }
+}
