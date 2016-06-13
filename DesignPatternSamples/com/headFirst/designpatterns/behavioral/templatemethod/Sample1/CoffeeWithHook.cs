de12b93ed4b35e4a07282c8dc8ad1f3660d6864a
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 19:35:28 +0530

    Added CoffeeWithHook class in Project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/CoffeeWithHook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    class CoffeeWithHook : CoffeeBeverageWithHook
+    {
+        protected override void addCondiments()
+        {
+            Console.WriteLine("Adding sugar and milk");
+        }
+
+        protected override void brew()
+        {
+            Console.WriteLine("Dripping coffee through filter");
+        }
+
+        protected override bool customerWantsCondiments()
+        {
+            var ans = getUserInput();
+
+            if (ans.ToLower().StartsWith("y"))
+                return true;
+            else
+                return false;
+        }
+
+        private string getUserInput()
+        {
+            var answer = string.Empty;
+
+            Console.Write("Would you like milk and sugar with your coffee (y/n)? ");
+
+            try
+            {
+                answer = Console.ReadLine();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("IO error trying to read your answer");
+            }
+
+            if (string.IsNullOrWhiteSpace(answer))
+                return "no";
+
+            return answer;
+        }
+    }
+}
