698aee96d14f8f808dba796dfebdfed686db058e
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 15:41:39 +0530

    Sample Project added for Template Method Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/TemplateMethodSample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    class TemplateMethodSample1
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+}
