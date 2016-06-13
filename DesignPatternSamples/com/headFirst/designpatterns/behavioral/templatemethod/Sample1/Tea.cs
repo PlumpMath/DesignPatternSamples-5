b604d346fbd52e5775e030edd8137fb434f46316
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 17:34:09 +0530

    Added Tea class in Project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/Tea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
+{
+    class Tea : CoffeeBeverage
+    {
+        protected override void addCondiments()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+
+        protected override void brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+    }
+}
