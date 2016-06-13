617b8ac27b5fa071311826671267723733df94f6
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-13 18:38:51 +0530

    Write a code to test CoffeeBeverage

--- DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/TemplateMethodSample1.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample1/TemplateMethodSample1.cs
@@ -6,6 +6,16 @@ namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample1
     {
         static void Main(string[] args)
         {
+            ICoffeeBeverage beverage = new Coffee();
+
+            Console.WriteLine("Making coffee...");
+            beverage.PrepareRecipe();
+
+            beverage = new Tea();
+
+            Console.WriteLine("Making tea...");
+            beverage.PrepareRecipe();
+
             Console.Read();
         }
     }
