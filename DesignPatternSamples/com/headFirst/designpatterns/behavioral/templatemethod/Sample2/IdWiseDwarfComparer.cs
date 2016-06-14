58e4ba5127fe79c185cbec41a1f0a88527abc4f9
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 14:45:53 +0530

    Added class IdWiseDwarfComparer

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/IdWiseDwarfComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
+{
+    class IdWiseDwarfComparer : IComparer<UncomparableDwarf>
+    {
+        public int Compare(UncomparableDwarf x, UncomparableDwarf y)
+        {
+            return x.Id.CompareTo(y.Id);
+        }
+    }
+}
