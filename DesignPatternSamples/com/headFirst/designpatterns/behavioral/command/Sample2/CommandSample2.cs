32964f5e074217ac74c7f8d3f2ed5dc4ff05fdcd
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 16:29:40 +0530

    Added logic to implement command pattern.

--- DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/CommandSample2.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/CommandSample2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Command;
+using Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Invoker;
+using System;
 
 namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2
 {
@@ -6,6 +8,24 @@ namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2
     {
         static void Main(string[] args)
         {
+            var fileSystemReceiver = FileSystemReceiverUtil.GetFileSystemReceiver();
+
+            var openFileCommand = new OpenFileCommand(fileSystemReceiver);
+
+            var closeFileCommand = new CloseFileCommand(fileSystemReceiver);
+
+            var readFileCommand = new ReadFileCommand(fileSystemReceiver);
+
+            var writeFileCommand = new WriteFileCommand(fileSystemReceiver);
+
+            var invoker = new FileInvoker();
+
+            invoker.Execute(openFileCommand);
+
+            invoker.Execute(closeFileCommand);
+
+            invoker.Execute(new ICommand[] { openFileCommand, readFileCommand, writeFileCommand, closeFileCommand });
+
             Console.Read();
         }
     }
