11cc0d9bb26f7485ab1cf81a5de721da37062c6a
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 15:54:55 +0530

    Added class WindowsFileSystemReceiver, which implements IFileSystemReceiver interface in the project.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/Receiver/WindowsFileSystemReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Receiver
+{
+    class WindowsFileSystemReceiver : IFileSystemReceiver
+    {
+        public void CloseFile()
+        {
+            Console.WriteLine("Closing file in windows OS");
+        }
+
+        public void OpenFile()
+        {
+            Console.WriteLine("Opening file in windows OS");
+        }
+
+        public void ReadFile()
+        {
+            Console.WriteLine("Reading file in windows OS");
+        }
+
+        public void WriteFile()
+        {
+            Console.WriteLine("Writing file in windows OS");
+        }
+    }
+}
