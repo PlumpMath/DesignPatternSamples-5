e8b03902eb7330e21834087f3eac40775e77c8ef
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 16:09:35 +0530

    Added FileInvoker class in the project, which is used to invoke command

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/Invoker/FileInvoker.cs
@@ -0,0 +1,18 @@
+﻿using Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Command;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Invoker
+{
+    class FileInvoker
+    {
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+        }
+
+        public void Execute(ICommand[] commands)
+        {
+            foreach (var command in commands)
+                command.Execute();
+        }
+    }
+}
