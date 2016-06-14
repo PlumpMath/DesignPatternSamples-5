dcd12fdd79af574c69160161bc067b3bda2ccd42
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 15:45:12 +0530

    Added Interface IFileSystemReceiver in project.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/Receiver/IFileSystemReceiver.cs
@@ -0,0 +1,13 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Receiver
+{
+    interface IFileSystemReceiver
+    {
+        void OpenFile();
+
+        void CloseFile();
+
+        void ReadFile();
+
+        void WriteFile();
+    }
+}
