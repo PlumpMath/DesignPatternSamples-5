9e9df63cab3ca8740e797f20bf94a8ad071e356e
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 16:24:21 +0530

    Added class FileSystemReceiverUtil class, which create instance of IFileSystemReceiver based on underlying OS.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/FileSystemReceiverUtil.cs
@@ -0,0 +1,32 @@
+﻿using Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Receiver;
+using System.Management;
+using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2
+{
+    class FileSystemReceiverUtil
+    {
+        public static IFileSystemReceiver GetFileSystemReceiver()
+        {
+            var osName = getOSFriendlyName();
+            Console.WriteLine(string.Concat("Underlaying OS is: ", osName));
+
+            if (osName.Contains("Windows"))
+                return new WindowsFileSystemReceiver();
+            else
+                return new UnixFileSystemReceiver();
+        }
+
+        static string getOSFriendlyName()
+        {
+            string result = string.Empty;
+            var searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (var os in searcher.Get())
+            {
+                result = os["Caption"].ToString();
+                break;
+            }
+            return result;
+        }
+    }
+}
