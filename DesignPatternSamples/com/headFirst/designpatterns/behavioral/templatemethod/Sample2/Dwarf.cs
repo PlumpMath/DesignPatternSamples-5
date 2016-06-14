fed7035dbe579782ef58cdea0b59315767e83e52
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 13:51:42 +0530

    Once we implement IComparable interfact to Dwarf, we are able to sort them. Otherwise Array.Sort() method throws InvalidOperationException

--- DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/Dwarf.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/Dwarf.cs
@@ -1,9 +1,23 @@
-﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
 {
-    sealed class Dwarf
+    sealed class Dwarf : IComparable
     {
         public string Name { get; set; }
 
         public int Id { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            var otherDwarf = obj as Dwarf;
+
+            if (otherDwarf != null)
+                return Name.CompareTo(otherDwarf.Name);
+            else
+                throw new ArgumentException("Object is not Dwarf");
+        }
     }
 }
