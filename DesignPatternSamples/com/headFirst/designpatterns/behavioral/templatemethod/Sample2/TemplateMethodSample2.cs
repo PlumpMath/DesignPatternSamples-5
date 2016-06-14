1c3c3cd3ea972df3a7ad8376864a0130f5776f76
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 13:45:19 +0530

    Create an array of Dwarf and try to sort them.

--- DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/TemplateMethodSample2.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/TemplateMethodSample2.cs
@@ -6,6 +6,29 @@ namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
     {
         static void Main(string[] args)
         {
+            var sevenDwarfs = new Dwarf[7];
+
+            sevenDwarfs[0] = new Dwarf() { Name = "Sleepy", Id = 1 };
+            sevenDwarfs[1] = new Dwarf() { Name = "Bashful", Id = 2 };
+            sevenDwarfs[2] = new Dwarf() { Name = "Doc", Id = 3 };
+            sevenDwarfs[3] = new Dwarf() { Name = "Dopey", Id = 4 };
+            sevenDwarfs[4] = new Dwarf() { Name = "Happy", Id = 5 };
+            sevenDwarfs[5] = new Dwarf() { Name = "Sneezy", Id = 6 };
+            sevenDwarfs[6] = new Dwarf() { Name = "Grumpy", Id = 7 };
+
+            Array.Sort(sevenDwarfs);
+
+            DisplayDwarfs(sevenDwarfs);
+
+            Console.Read();
+        }
+
+        static void DisplayDwarfs(Dwarf[] dwarfs)
+        {
+            foreach(var dwarf in dwarfs)
+            {
+                Console.WriteLine("{0}\t{1}", dwarf.Name, dwarf.Id);
+            }
         }
     }
 }
