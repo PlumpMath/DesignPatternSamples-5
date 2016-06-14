31b4134b44ed557b13f8448c1da332492d958326
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 15:49:16 +0530

    Added UnixFileSystemReceiver class, which implements IFileSystemReceiver interface in the project.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/command/Sample2/Receiver/UnixFileSystemReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Command.Sample2.Receiver
+{
+    class UnixFileSystemReceiver : IFileSystemReceiver
+    {
+        public void CloseFile()
+        {
+            Console.WriteLine("Closing file in unix OS");
+        }
+
+        public void OpenFile()
+        {
+            Console.WriteLine("Opening file in unix OS");
+        }
+
+        public void ReadFile()
+        {
+            Console.WriteLine("Reading file in unix OS");
+        }
+
+        public void WriteFile()
+        {
+            Console.WriteLine("Writing file in unix OS");
+        }
+    }
+}
