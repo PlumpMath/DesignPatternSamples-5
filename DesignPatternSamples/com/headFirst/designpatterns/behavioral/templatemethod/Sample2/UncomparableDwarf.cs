667bfe7eddbf68db21abb595b7dad866ed562901
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 14:30:38 +0530

    Added new class UncomparableDwarf, which will not implement IComparable interface.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/UncomparableDwarf.cs
@@ -0,0 +1,9 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
+{
+    sealed class UncomparableDwarf
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
