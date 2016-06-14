a9764297b9c86b5021286a3907bae3209e674286
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 13:37:38 +0530

    Added a Dwarf class

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/Dwarf.cs
@@ -0,0 +1,9 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
+{
+    sealed class Dwarf
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
