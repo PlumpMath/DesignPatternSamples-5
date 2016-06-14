55ffafac30fc8508bb4251743b0b6c143834f588
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 14:39:02 +0530

    Class Added named NamewiseDwarfComparer which compare two dwarf object bases on their name property

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/NamewiseDwarfComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
+{
+    class NamewiseDwarfComparer : IComparer<UncomparableDwarf>
+    {
+        public int Compare(UncomparableDwarf x, UncomparableDwarf y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
