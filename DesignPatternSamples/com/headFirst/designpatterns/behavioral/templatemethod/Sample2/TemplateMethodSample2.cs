60d66dc909b628715fd4803d42a5f33da2a634a1
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-14 14:43:45 +0530

    Implement logic to sort UncomparableDwarf array name wise using Array.Sort method

--- DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/TemplateMethodSample2.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/templatemethod/Sample2/TemplateMethodSample2.cs
@@ -21,12 +21,34 @@ namespace Com.HeadFirst.DesignPatterns.Behavioral.TemplateMethod.Sample2
 
             DisplayDwarfs(sevenDwarfs);
 
+            var sevenUncomparableDwarfs = new UncomparableDwarf[7];
+
+            sevenUncomparableDwarfs[0] = new UncomparableDwarf() { Name = "Sleepy", Id = 1 };
+            sevenUncomparableDwarfs[1] = new UncomparableDwarf() { Name = "Bashful", Id = 2 };
+            sevenUncomparableDwarfs[2] = new UncomparableDwarf() { Name = "Doc", Id = 3 };
+            sevenUncomparableDwarfs[3] = new UncomparableDwarf() { Name = "Dopey", Id = 4 };
+            sevenUncomparableDwarfs[4] = new UncomparableDwarf() { Name = "Happy", Id = 5 };
+            sevenUncomparableDwarfs[5] = new UncomparableDwarf() { Name = "Sneezy", Id = 6 };
+            sevenUncomparableDwarfs[6] = new UncomparableDwarf() { Name = "Grumpy", Id = 7 };
+
+            Array.Sort(sevenUncomparableDwarfs, new NamewiseDwarfComparer());
+
+            DisplayUncomaparableDwarfs(sevenUncomparableDwarfs);
+
             Console.Read();
         }
 
         static void DisplayDwarfs(Dwarf[] dwarfs)
         {
             foreach(var dwarf in dwarfs)
+            {
+                Console.WriteLine("{0}\t{1}", dwarf.Name, dwarf.Id);
+            }
+        }
+
+        static void DisplayUncomaparableDwarfs(UncomparableDwarf[] dwarfs)
+        {
+            foreach (var dwarf in dwarfs)
             {
                 Console.WriteLine("{0}\t{1}", dwarf.Name, dwarf.Id);
             }
