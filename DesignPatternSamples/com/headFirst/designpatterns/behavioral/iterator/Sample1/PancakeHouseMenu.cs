bfcf3b253590119bbc64ea1291572b9e3dcb60ee
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-16 14:38:17 +0530

    Added PancakeHouseMenu class, which uses ArrayList to store its MenuItem

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample1/PancakeHouseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample1
+{
+    class PancakeHouseMenu
+    {
+        ArrayList _menuItems;
+
+        public PancakeHouseMenu()
+        {
+            _menuItems = new ArrayList();
+
+            AddItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
+
+            AddItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+
+            AddItem("Blueberry Pancakes", "Pancakes made with fresh blueberries, and blueberry syrup", true, 3.49);
+
+            AddItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+        }
+
+        public void AddItem(string name, string description, bool vegeterian, double price)
+        {
+            var item = new MenuItem(name, description, vegeterian, price);
+            _menuItems.Add(item);
+        }
+
+        public ArrayList GetMenuItems()
+        {
+            return _menuItems;
+        }
+    }
+}
