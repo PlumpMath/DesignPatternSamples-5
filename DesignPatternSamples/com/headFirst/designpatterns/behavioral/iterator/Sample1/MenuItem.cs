7e4af63d90a1b7dda84196e2369421ae159cc523
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-16 14:16:29 +0530

    Added class MenuItem in the priject

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample1/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample1
+{
+    class MenuItem
+    {
+        public MenuItem(string name, string description, bool vegeterian, double price)
+        {
+            Name = name;
+            Description = description;
+            Vegeterian = vegeterian;
+            Price = price;          
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public bool Vegeterian { get; private set; }
+
+        public double Price { get; private set; }
+    }
+}
