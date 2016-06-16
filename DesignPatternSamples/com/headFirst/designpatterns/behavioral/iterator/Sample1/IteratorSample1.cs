836791e83035ba3f4b16eb8f2e52c420841ad58f
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-16 13:54:09 +0530

    Added Sample Project for Iterator Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample1/IteratorSample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample1
+{
+    class IteratorSample1
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+}
