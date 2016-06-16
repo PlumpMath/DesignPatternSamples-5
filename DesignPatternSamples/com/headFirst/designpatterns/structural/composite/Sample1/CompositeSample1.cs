d4db70b5e5a203c769b64d450009928645682665
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-16 20:05:27 +0530

    Added Project for Composite Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/structural/composite/Sample1/CompositeSample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Structural.Composite.Sample1
+{
+    class CompositeSample1
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+}
