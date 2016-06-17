14d5a0dbbeab25a6d04e85e2315a253951a0c760
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-17 14:42:20 +0530

    Now Implement IEnumerable<MenuComponent> to provide external iterator functionality

--- DesignPatternSamples/com/headFirst/designpatterns/structural/composite/Sample1/MenuComponent.cs
+++ DesignPatternSamples/com/headFirst/designpatterns/structural/composite/Sample1/MenuComponent.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Com.HeadFirst.DesignPatterns.Structural.Composite.Sample1
 {
-    abstract class MenuComponent
+    abstract class MenuComponent : IEnumerable<MenuComponent>
     {
         protected MenuComponent(string name, string description)
         {
@@ -34,5 +36,12 @@ namespace Com.HeadFirst.DesignPatterns.Structural.Composite.Sample1
         public virtual double Price { get { throw new InvalidOperationException(); } protected set { } }
 
         public abstract void Print();
+
+        public abstract IEnumerator<MenuComponent> GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
     }
 }
