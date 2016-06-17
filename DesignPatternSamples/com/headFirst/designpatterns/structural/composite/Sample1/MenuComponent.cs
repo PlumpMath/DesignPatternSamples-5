a72855e9897ed05baefea5bd0f7b0d12b5cea90a
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-17 13:04:04 +0530

    Added abstact class MenuComponent as a base Composit Component

---
+++ DesignPatternSamples/com/headFirst/designpatterns/structural/composite/Sample1/MenuComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Structural.Composite.Sample1
+{
+    abstract class MenuComponent
+    {
+        protected MenuComponent(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public virtual void Add(MenuComponent menuComponent)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public virtual void Remove(MenuComponent menuComponent)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public virtual MenuComponent GetChild(int position)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public virtual bool Vegetarian { get { throw new InvalidOperationException(); } protected set { } }
+
+        public virtual double Price { get { throw new InvalidOperationException(); } protected set { } }
+
+        public abstract void Print();
+    }
+}
