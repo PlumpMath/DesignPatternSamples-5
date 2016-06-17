8345d471f6f469950a6fb310927279fbdc7c7c4a
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-17 13:12:55 +0530

    Adding MenuItem class which serves as Leaf in Composit structure

---
+++ DesignPatternSamples/com/headFirst/designpatterns/structural/composite/Sample1/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Structural.Composite.Sample1
+{
+    class MenuItem : MenuComponent
+    {
+        public MenuItem(string name, string description, bool vegetarian, double price)
+            :base(name, description)
+        {
+            Vegetarian = vegetarian;
+            Price = price;
+        }
+
+        public override bool Vegetarian { get; protected set; }
+
+        public override double Price { get; protected set; }
+
+        public override void Print()
+        {
+            Console.Write(string.Concat(" ", Name));
+            if (Vegetarian)
+                Console.Write("(v)");
+            Console.Write(string.Concat(", ", Price));
+            Console.WriteLine(string.Concat("   -- ", Description));
+        }
+    }
+}
