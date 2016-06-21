00ccf80c3fc2d5f00f35eb6f8c369671831e0bf4
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-21 13:10:23 +0530

    Added Context component of State Pattern, Account class.

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/state/Sample1/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.State.Sample1
+{
+    class Account
+    {
+        double _balance = 0;
+        IAccountState _nilAcountState;
+        IAccountState _silverAccountState;
+        IAccountState _goldAccountState;
+
+        IAccountState _currentState;
+
+        public Account(double balance)
+        {
+            _balance = balance;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            _currentState.Deposit(amount);
+            _balance = _currentState.GetBalance();
+        }
+
+        public void Withdraw(double amount)
+        {
+            _currentState.Withdraw(amount);
+            _balance = _currentState.GetBalance();
+        }
+
+        public void PayInterest()
+        {
+            _currentState.PayInterest();
+            _balance = _currentState.GetBalance();
+        }
+
+        public IAccountState GetNilState()
+        {
+            
+        }
+
+        public IAccountState GetSilverState()
+        {
+
+        }
+
+        public IAccountState GetGoldState()
+        {
+
+        }
+    }
+}
