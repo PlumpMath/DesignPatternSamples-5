6bbd71590923414df76d3eeada57e5bbc69df356
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-21 17:49:02 +0530

    Added IState interface

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/state/Sample2/IState.cs
@@ -0,0 +1,15 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.State.Sample2
+{
+    interface IState
+    {
+        void InsertQuarter();
+
+        void EjectQuarter();
+
+        void TurnCrank();
+
+        void Dispense();
+
+        void Refill();
+    }
+}
