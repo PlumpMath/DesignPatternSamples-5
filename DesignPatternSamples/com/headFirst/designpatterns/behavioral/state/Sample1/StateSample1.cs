9f48b945df9720edaad4eb117372ecefd7520450
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-21 12:16:03 +0530

    Project Added for State Design Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/state/Sample1/StateSample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.State.Sample1
+{
+    class StateSample1
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+}
