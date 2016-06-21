003fa90ce67dccfd73733c7146da9d6ec2d894f8
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-21 12:21:18 +0530

    Created IAccountState interface to implement State Patterns

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/state/Sample1/IAccountState.cs
@@ -0,0 +1,13 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.State.Sample1
+{
+    interface IAccountState
+    {
+        double GetBalance();
+
+        void Deposit(double amount);
+
+        void Withdraw(double amount);
+
+        void PayInterest();
+    }
+}
