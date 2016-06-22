68a7f227cbf8629d2da133427325668aac10efaa
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-22 18:24:18 +0530

    Added class Safari, which implement Array to manage its Animal collection

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample2/Safari.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample2
+{
+    class Safari
+    {
+        Animal[] _animals;
+        readonly int MAX_LIMIT = 6;
+
+        public Safari()
+        {
+            _animals = new Animal[MAX_LIMIT];
+            AddAnimal("Buffelow", false);
+            AddAnimal("Rhino", true);
+            AddAnimal("Bear", true);
+            AddAnimal("Elephant", false);
+        }
+
+        public Animal[] GetAnimals()
+        {
+            return _animals;
+        }
+
+        public void AddAnimal(string name, bool wild)
+        {
+            var item = new Animal(name, wild);
+
+            if (_animals.Length >= MAX_LIMIT)
+                Console.WriteLine("Sorry!! can't add more animal");
+            else
+            {
+                _animals.SetValue(item, _animals.Length);
+            }
+        }
+    }
+}
