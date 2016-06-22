45a4f498f2c344596a577a46297415859cfa9557
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-22 16:57:17 +0530

    Added Animal class into the project

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample2/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample2
+{
+    class Animal
+    {
+        public Animal(string name, bool wild)
+        {
+            Name = name;
+            Wild = wild;
+        }
+
+        public string Name { get; private set; }
+
+        public bool Wild { get; private set; }
+    }
+}
