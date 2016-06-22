7bc900b4284029a07057e08c14f8e00d6e184d7c
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-22 18:13:34 +0530

    Added Zoo class, which uses Arraylist to manage its animal collection

---
+++ DesignPatternSamples/com/headFirst/designpatterns/behavioral/iterator/Sample2/Zoo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.HeadFirst.DesignPatterns.Behavioral.Iterator.Sample2
+{
+    class Zoo
+    {
+        ArrayList _animals;
+
+        public Zoo()
+        {
+            _animals = new ArrayList();
+            AddAnimal("African Loin", true);
+            AddAnimal("Orang utang", true);
+            AddAnimal("Zibra", true);
+            AddAnimal("Royal Bengal Tiger", true);
+            AddAnimal("Asian Elephant", false);
+            AddAnimal("Hippopotamas", false);
+            AddAnimal("Spotted Deer", false);
+            AddAnimal("Wolf", true);
+        }
+
+        public ArrayList GetAnimals()
+        {
+            return _animals;
+        }
+
+        public void AddAnimal(string name, bool wild)
+        {
+            var item = new Animal(name, wild);
+            _animals.Add(item);
+        }
+    }
+}
