54906ac98eb95ef888a185c5b6861d6ef7f8eeaf
Himanshu <himanshu.manjarawala@automationanywhere.com>
2016-06-30 18:56:20 +0530

    Added Project for Abstract Factory Pattern

---
+++ DesignPatternSamples/com/headFirst/designpatterns/creational/absfactory/Sample2/AbstractFactorySample2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.HeadFirst.DesignPatterns.Creational.AbstractFactory.Sample2
+{
+    class AbstractFactorySample2
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+        }
+    }
+}
